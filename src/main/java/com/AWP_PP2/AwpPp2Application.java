9a89ecdca6e54421d9e8bdaf4c5a8c6e717770bf
DELL <DELL@PaulaSalas>
2023-06-06 01:48:53 -0600

    Practica #2

---
+++ src/main/java/com/AWP_PP2/AwpPp2Application.java
@@ -0,0 +1,13 @@
+package com.AWP_PP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwpPp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwpPp2Application.class, args);
+	}
+
+}
